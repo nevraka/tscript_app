6c62703833e4a7d545d298aaf11db25e693fb2c7
nevraka <nevrakaya@gmail.com>
2021-02-23 21:15:33 +0300

    create next app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+//import Link from 'next/link'
+import Layout from '../components/Layout';
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+    <div className="flex rounded-lg p-24 justify-center">
+      <img
+        className="h-24 w-24 rounded-full mx-0 mr-5"
+        src="https://bit.ly/37LLS6l"
+      />
+      <div className="text-center md:text-left">
+        <h2 className="text-lg font-bold">Jake Prins</h2>
+        <div className="text-purple-600">JavaScript developer</div>
+        <div className="text-gray-600">Twitter: @jakeprins_nl</div>
+        <div className="text-gray-600">jakeprins.com</div>
+      </div>
+    </div>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
