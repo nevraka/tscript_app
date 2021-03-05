aa2108eaeb5a932cd824e96e95a6dc550a821251
nevraka <nevrakaya@gmail.com>
2021-03-05 14:33:11 +0300

    heading improvoment

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,112 @@
 const Header = () => {
   return (
-    <div className="hidden sm:block sm:ml-6">
-      <button className="block h-6 w-8">
-        <img
-          className="h-full w-full object-cover border-2"
-          src="https://www.monologuestories.com/wp-content/plugins/under-construction-page/themes/plain_text/monologue-stories-logo.jpg"
-          alt="avatar"
-        ></img>
-      </button>
-      <div>
-        <a href="#">Bardaklar</a>
-        <a href="#">Tabaklar</a>
-        <a href="#">Mumluklar</a>
-        <a href="#">Vazolar</a>
+    <div className="flex sm:ml-6 ">
+      <div className="pt-10 py-1 block self-center">
+        <div className=" inline-flex font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+          <button
+            id="options-menu"
+            aria-haspopup="true"
+            aria-expanded="true"
+            className="pl-5 pr-5 flex"
+            role="menu"
+          >
+            Bardaklar
+            <svg
+              className="-mr-1 ml-2 h-5 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="inline-flex font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+          <button
+            id="options-menu"
+            aria-haspopup="true"
+            aria-expanded="true"
+            className="pl-5 pr-5 flex"
+            type="button"
+            role="menu"
+          >
+            Tabaklar
+            <svg
+              className="-mr-1 ml-2 h-5 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="inline-flex font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+          <button
+            id="options-menu"
+            aria-haspopup="true"
+            aria-expanded="true"
+            className="pl-5 pr-5 flex"
+            type="button"
+            role="menu"
+          >
+            Mumluklar
+            <svg
+              className="-mr-1 ml-2 h-5 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="inline-flex font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+          <button
+            id="options-menu"
+            aria-haspopup="true"
+            aria-expanded="true"
+            className="pl-5 pr-5 flex"
+            type="button"
+            role="menu"
+          >
+            Vazolar
+            <svg
+              className="-mr-1 ml-2 h-5 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
+      <img
+        className="h-full content-center block h-6 w-8  pt-10 h-auto w-100px "
+        src="https://www.monologuestories.com/wp-content/uploads/2020/05/monologue-stories-logo.jpg"
+        alt="avatar"
+      ></img>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,23 @@
-//import Link from 'next/link'
-import Layout from '../components/Layout';
+import Header from '../components/Header';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <div className="flex rounded-lg p-24 justify-center">
-      <img
-        className="h-24 w-24 rounded-full mx-0 mr-5"
-        src="https://bit.ly/37LLS6l"
-      />
-      <div className="text-center md:text-left">
-        <h2 className="text-lg font-bold">Jake Prins</h2>
-        <div className="text-purple-600">JavaScript developer</div>
-        <div className="text-gray-600">Twitter: @jakeprins_nl</div>
-        <div className="text-gray-600">jakeprins.com</div>
+const IndexPage = () => {
+  return (
+    <>
+      <Header />
+      <div className="flex  p-24 justify-center ">
+        <img
+          className="h-24 w-24 rounded-full mx-0 mr-5"
+          src="https://bit.ly/37LLS6l"
+        />
+        <div className="text-center md:text-left">
+          <h2 className="text-lg font-bold">Jake Prins</h2>
+          <div className="text-purple-600">JavaScript developer</div>
+          <div className="text-gray-600">Twitter: @jakeprins_nl</div>
+          <div className="text-gray-600">jakeprins.com</div>
+        </div>
       </div>
-    </div>
-  </Layout>
-);
+    </>
+  );
+};
 
 export default IndexPage;
