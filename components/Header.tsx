0ffad342de03f1d8e1af9552f2b3a412f36b1b3a
nevraka <nevrakaya@gmail.com>
2021-03-11 16:36:05 +0300

    fontawasome design change

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,6 @@
+import { faAngleDown, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const Header = () => {
   return (
     <div className="flex sm:ml-6 ">
@@ -11,19 +14,10 @@ const Header = () => {
             role="menu"
           >
             Bardaklar
-            <svg
-              className="-mr-1 ml-2 h-5 w-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              />
-            </svg>
+            <FontAwesomeIcon
+              style={{ alignSelf: 'center', margin: '5px' }}
+              icon={faAngleDown}
+            />
           </button>
         </div>
         <div className="inline-flex font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
@@ -36,19 +30,10 @@ const Header = () => {
             role="menu"
           >
             Tabaklar
-            <svg
-              className="-mr-1 ml-2 h-5 w-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              />
-            </svg>
+            <FontAwesomeIcon
+              icon={faAngleDown}
+              style={{ alignSelf: 'center', margin: '5px' }}
+            />
           </button>
         </div>
         <div className="inline-flex font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
@@ -61,19 +46,10 @@ const Header = () => {
             role="menu"
           >
             Mumluklar
-            <svg
-              className="-mr-1 ml-2 h-5 w-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              />
-            </svg>
+            <FontAwesomeIcon
+              icon={faAngleDown}
+              style={{ alignSelf: 'center', margin: '5px' }}
+            />
           </button>
         </div>
         <div className="inline-flex font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
@@ -86,27 +62,24 @@ const Header = () => {
             role="menu"
           >
             Vazolar
-            <svg
-              className="-mr-1 ml-2 h-5 w-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              />
-            </svg>
+            <FontAwesomeIcon
+              icon={faAngleDown}
+              style={{ alignSelf: 'center', margin: '5px' }}
+            />
           </button>
         </div>
       </div>
       <img
-        className="h-full content-center block h-6 w-8  pt-10 h-auto w-100px "
-        src="https://www.monologuestories.com/wp-content/uploads/2020/05/monologue-stories-logo.jpg"
+        className="h-full content-center block h-20 w-24  pt-10 h-auto w-100px "
+        src="https://cdn.shopier.net/fetch.php?p=logo_468/yakaart12_alt5.jpg"
         alt="avatar"
       ></img>
+      <div className="pt-10 px-8 py-1 block self-center">
+        <form>
+          <FontAwesomeIcon style={{ marginRight: '20px' }} icon={faSearch} />
+          <input type="text" placeholder="Search.." name="search" />
+        </form>
+      </div>
     </div>
   );
 };
