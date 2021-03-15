fcb7c7bcd39e1504e24292e01e909391959dfc88
nevraka <nevrakaya@gmail.com>
2021-03-15 22:52:55 +0300

    search css

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Header = () => {
   return (
-    <div className="flex sm:ml-6 ">
-      <div className="pt-10 py-1 block self-center">
+    <div className="flex sm:ml-6 pt-10">
+      <div className="py-1 block self-center">
         <div className=" inline-flex font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
           <button
             id="options-menu"
@@ -70,15 +70,22 @@ const Header = () => {
         </div>
       </div>
       <img
-        className="h-full content-center block h-20 w-24  pt-10 h-auto w-100px "
+        className="h-full content-center block h-20 w-24 h-auto w-100px "
         src="https://cdn.shopier.net/fetch.php?p=logo_468/yakaart12_alt5.jpg"
         alt="avatar"
       ></img>
-      <div className="pt-10 px-8 py-1 block self-center">
-        <form>
+      <div className="dropdown table relative mx-8 self-center ml-auto">
+        <button className="block self-center">
           <FontAwesomeIcon style={{ marginRight: '20px' }} icon={faSearch} />
-          <input type="text" placeholder="Search.." name="search" />
-        </form>
+        </button>
+        <div className="dropdown-menu px-10 py-8 shadow-lg border-2 border-solid border-gray-200 absolute right-0 hidden text-gray-700 pt-1">
+          <input
+            className="outline-none border border-solid px-2 py-1"
+            type="text"
+            placeholder="Search.."
+            name="search"
+          />
+        </div>
       </div>
     </div>
   );
